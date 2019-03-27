eb1122604b071a5bdbab9eea7e3ae3b7b0b76699
Evgeniy <7850039+own2pwn@users.noreply.github.com>
2019-03-27 17:16:14 +0300

    swift 5 & xcode fix

--- Sources/Types & Operators/Vector.swift
+++ Sources/Types & Operators/Vector.swift
@@ -58,16 +58,16 @@ extension VectorType {
     }
 }
 
-public protocol SIMDVectorType {
-    static func * (lhs: Self, rhs: Double) -> Self
-    static func / (lhs: Self, rhs: Double) -> Self
-}
-
-extension SIMDVectorType {
-    public static func / (lhs: Self, rhs: Double) -> Self {
-        return lhs * (1 / rhs)
-    }
-}
+//public protocol SIMDVectorType {
+//    static func * (lhs: Self, rhs: Double) -> Self
+//    static func / (lhs: Self, rhs: Double) -> Self
+//}
+//
+//extension SIMDVectorType {
+//    public static func / (lhs: Self, rhs: Double) -> Self {
+//        return lhs * (1 / rhs)
+//    }
+//}
 
 extension Double: VectorType, VectorConvertible {
     public static var zero = 0.0
@@ -84,7 +84,7 @@ extension Double: VectorType, VectorConvertible {
     }
 }
 
-extension double2: VectorType, VectorConvertible, SIMDVectorType {
+extension double2: VectorType, VectorConvertible/*, SIMDVectorType*/ {
     public static var zero = double2()
     public static var rawValueCount: Int { return 2 }
     public func distance(between: double2) -> Double {
@@ -92,7 +92,7 @@ extension double2: VectorType, VectorConvertible, SIMDVectorType {
     }
 }
 
-extension double3: VectorType, VectorConvertible, SIMDVectorType {
+extension double3: VectorType, VectorConvertible/*, SIMDVectorType */ {
     public static var zero = double3()
     public static var rawValueCount: Int { return 3 }
     public func distance(between: double3) -> Double {
@@ -100,7 +100,7 @@ extension double3: VectorType, VectorConvertible, SIMDVectorType {
     }
 }
 
-extension double4: VectorType, VectorConvertible, SIMDVectorType {
+extension double4: VectorType, VectorConvertible/*, SIMDVectorType */ {
     public static var zero = double4()
     public static var rawValueCount: Int { return 4 }
     public func distance(between: double4) -> Double {
